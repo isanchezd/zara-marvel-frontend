aa05b1c965195751ade4850053cb7c74828e98b0
Iván Sánchez <sanchez.diaz.ivan@gmail.com>
2024-04-13 17:42:24 +0200

    feat: integred heroList and fixed general layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import SearchBar from '@/app/components/search-bar';
+import HeroList from './components/hero-list';
 
 export default function Home() {
   return (
-    <SearchBar />
-  );
+    <>
+      <SearchBar />
+      <HeroList />
+    </>
+  )
 }
