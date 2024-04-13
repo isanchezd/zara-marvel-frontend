b6412bef3c97b20ea5054369cae6a64b3a521d9c
Iván Sánchez <sanchez.diaz.ivan@gmail.com>
2024-04-13 19:45:25 +0200

    feat: basic layout for hero banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import SearchBar from '@/app/components/search-bar';
-import HeroList from './components/hero-list';
+import HeroList from '@/app/components/hero-list';
+import styles from '@/app/page.module.css';
 
 export default function Home() {
   return (
-    <>
+    <section className={`${styles.heroesLayout}`}>
       <SearchBar />
       <HeroList />
-    </>
+    </section>
   )
 }
