a3a872100077587a66ac4ad9d851e3dc0146539e
Iván Sánchez <sanchez.diaz.ivan@gmail.com>
2024-04-14 09:21:42 +0200

    feat: added styles to card

--- src/app/components/hero-list/hero-card/hero-card.tsx
+++ src/app/components/hero-list/hero-card/hero-card.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react';
 import Hero from '@/domain/Hero';
 import styles from './hero-card.module.css';
@@ -18,8 +20,22 @@ export default function HeroCard({ hero }: HeroCardProps) {
         </header>
         <section className={`${styles.cardContent}`}>
           <span className={`${styles.name}`}>{hero.name}</span>
-          <img className={`${styles.favorite}`} src='/icon-heart-default.png' alt='add-favorite' />
+          <svg
+            width='13'
+            height='12'
+            viewBox='0 0 13 12'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+            className={`${styles.iconFavorite}`}
+          >
+            <path
+              fillRule='evenodd'
+              clipRule='evenodd'
+              d='M6.57153 2.37318L3.57153 0.552368L0.571533 2.37318V6.27491L6.57153 11.3905L12.5715 6.27491V2.37318L9.57154 0.552368L6.57153 2.37318Z'
+  
+            />
+          </svg>
         </section>
-      </article> 
+      </article>
     )
 }
