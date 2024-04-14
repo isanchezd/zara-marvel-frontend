b586bfcb89f8e0565f3cf49c42beff0ffdbc12f0
Iván Sánchez <sanchez.diaz.ivan@gmail.com>
2024-04-14 10:46:02 +0200

    feat: updated repository for include a name value

--- src/useCases/getHeroes.ts
+++ src/useCases/getHeroes.ts
@@ -1,5 +1,5 @@
 import HeroRepository from '@/domain/HeroRepository';
 
-export default function(repository: HeroRepository) {
-    return repository.getHeroes();
+export default function(repository: HeroRepository, name?: string) {
+    return repository.getHeroes(name);
 }
